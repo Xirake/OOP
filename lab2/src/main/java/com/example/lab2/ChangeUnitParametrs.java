8488012550ac2fc750a56b6843a03c30368aec13
Xirake <121200643+Xirake@users.noreply.github.com>
2023-04-10 12:22:24 +0300

    lab2

---
+++ lab2/src/main/java/com/example/lab2/ChangeUnitParametrs.java
@@ -0,0 +1,105 @@
+package com.example.lab2;
+
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class ChangeUnitParametrs {
+    public static void change_parametrs(ArrayList<String> paramsTochange) {
+        Stage window = new Stage();
+        Label nameLabel=new Label();
+        nameLabel.setText("Name:");
+        TextField nameText = new TextField();
+        nameText.setText(paramsTochange.get(1));
+
+        Label healthLabel=new Label();
+        healthLabel.setText("Health:");
+        TextField healthText = new TextField();
+        healthText.setText(paramsTochange.get(2));
+
+        Label xLabel=new Label();
+        xLabel.setText("X:");
+        TextField xText = new TextField();
+        xText.setText(paramsTochange.get(3));
+
+        Label yLabel=new Label();
+        yLabel.setText("Y:");
+        TextField yText = new TextField();
+        yText.setText(paramsTochange.get(4));
+
+        VBox layout = new VBox(11);
+        layout.setAlignment(Pos.CENTER);
+        Button okButton=new Button("OK");
+        okButton.setOnAction(e->{
+
+            String sName= nameText.getText();
+            String sHealth= healthText.getText();
+            String sX = xText.getText();
+            String sY = yText.getText();
+            int sID = Integer.parseInt(paramsTochange.get(0));
+            HelloApplication.changeSoldier(sID, sName, sHealth, sX, sY);
+
+            window.close(); });
+
+
+        layout.getChildren().addAll(nameLabel, nameText, healthLabel, healthText, xLabel, xText, yLabel, yText, okButton);
+
+        Scene scene=new Scene(layout,303,300);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+    public static void display(double x, double y) {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Уведіть параметри нового солдату!");
+        window.setMinWidth(250);
+        ArrayList<String> units = HelloApplication.getnames();
+
+        ComboBox cBox = new ComboBox();
+        int count = 0;
+        for (String s:units){
+            cBox.getItems().add(Integer.toString(count)+" "+ s);
+            count++;
+        }
+        VBox layout = new VBox(10);
+        layout.setAlignment(Pos.CENTER);
+
+        Label label = new Label("Choose object for change or delete: ");
+
+        Button okButton = new Button("OK");
+        okButton.setOnAction(e->{
+            if (cBox.getValue()!=null) {
+
+                String[] strChoice = cBox.getValue().toString().split(" ");
+                ArrayList<String> UnitInfo = HelloApplication.getParamsToChange( Integer.parseInt(strChoice[0]) );
+                System.out.println("Було обрано для зміни об'єкт "+ UnitInfo);
+                change_parametrs(UnitInfo);
+            }
+            window.close();
+        });
+        Button deleteButton = new Button("Delete");
+        deleteButton.setOnAction(e->{
+            if (cBox.getValue()!=null) {
+                String[] strChoice = cBox.getValue().toString().split(" ");
+                int ID = Integer.parseInt(strChoice[0]);
+                HelloApplication.DeleteUnit(ID);
+                System.out.println("Було видалено об'єкт " + ID);
+            }
+            window.close();
+        });
+        layout.getChildren().addAll(label, cBox, okButton, deleteButton);
+        Scene scene = new Scene(layout, 303, 303);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+}
---
+++ lab2/src/main/java/com/example/lab2/HelloApplication.java
@@ -0,0 +1,234 @@
+package com.example.lab2;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import java.io.IOException;
+class Soldier {
+    private Label name;
+
+    private double hp;
+    private Line life;
+    private ImageView isoldier;
+    private double x, y;
+    private int ID;
+    private Rectangle rectActive;
+    public Soldier(String soldier_name, double h, double _x, double _y){
+        x = _x - 80;
+        y = _y-70;
+        name=new Label(soldier_name);
+        name.setLayoutX(x);
+        name.setLayoutY(y);
+        hp = h;
+        life = new Line(x+5, y, x+105, y);
+        life.setStrokeWidth(6);
+        life.setStroke(Color.GREEN);
+        isoldier = new ImageView(HelloApplication.imsoldier);
+        isoldier.setX(x);
+        isoldier.setY(y);
+        ID = HelloApplication.counter;
+        HelloApplication.group.getChildren().addAll(name, life, isoldier);
+
+
+        rectActive= new Rectangle(x-5,y-5,105,105);
+        rectActive.setFill(Color.TRANSPARENT);
+        rectActive.setStroke(Color.MAGENTA);
+
+    }
+    public void delete(){
+        HelloApplication.group.getChildren().removeAll(name, life, isoldier);
+
+    }
+    public String getName(){return name.getText();}
+    public String getHealth(){return Double.toString(hp);}
+    public void setName(String n){name.setText(n);}
+    public void setHealth(String h){
+
+        try {
+            hp = Double.parseDouble(h);
+        }
+        catch(Exception e){
+            hp=0.0;
+        }
+
+        life.setStartX(x);
+        life.setStartY(y+15);
+        life.setEndX(x+hp);
+        life.setEndY(y+15);
+    }
+    public String getID(){return Integer.toString(ID);}
+
+
+    public void setCoordinates(){
+        name.setLayoutX(x);
+        name.setLayoutY(y);
+
+
+        life.setStartX(x);
+        life.setStartY(y+15);
+        life.setEndX(x+hp);
+        life.setEndY(y+15);
+        isoldier.setX(x);
+        isoldier.setY(y+15+7);
+
+        rectActive.setX(x-5);
+        rectActive.setY(y-5);
+
+    }
+
+    public String getX(){
+        return Double.toString(x);
+    }
+
+    public void setX( double _x ){
+        x= _x;
+
+        setCoordinates();
+    }
+    public void setX(String sX){
+        try {
+            x= Double.parseDouble(sX);
+        }
+        catch(Exception e){
+            x=0.0;
+        }
+
+        setX(x);
+    }
+    public String getY(){
+        return Double.toString(y);
+    }
+
+    public void setY( double _y ){
+        y= _y;
+
+        setCoordinates();
+    }
+
+    public void setY(String sY) {
+        try {
+            y = Double.parseDouble(sY);
+        } catch (Exception e) {
+            y = 0.0;
+        }
+
+        setY(y);
+    }
+    
+}
+
+
+
+public class HelloApplication extends Application {
+    public static void DeleteUnit(int index){
+        Soldier r = herd.get(index);
+        herd.remove(index);
+        r.delete();
+
+    }
+
+    public static ArrayList<String> getParamsToChange(int index){
+        Soldier r = herd.get(index);
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add(r.getID());
+        arr.add( r.getName() );
+        arr.add( r.getHealth() );
+        arr.add( r.getX() );
+        arr.add( r.getY() );
+        System.out.println(arr);
+        return arr;
+    }
+    public static void changeSoldier(int SoldierIndex, String sName, String sHealth, String sX, String sY ){
+        Soldier r = herd.get(SoldierIndex);
+
+        r.setName(sName);
+        r.setHealth(sHealth);
+        r.setX(sX);
+        r.setY(sY);
+
+    }
+    public static void moving (double _x, double _y){
+        int SoldierIndex = 0;
+        Soldier r= herd.get(SoldierIndex);
+        r.setX(Double.parseDouble(r.getX())+_x);
+        r.setY(Double.parseDouble(r.getY())+_y);
+    }
+    public static Group group = new Group();
+    public static Image imsoldier;
+    public static ArrayList<Soldier> herd = new ArrayList<>();
+    public static int counter = 0;
+    public static ArrayList<String> getnames() {
+        ArrayList<String> arr = new ArrayList<>();
+        for (Soldier r : HelloApplication.herd) {
+            arr.add(r.toString());
+        }
+        return arr;
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        imsoldier = new Image(HelloApplication.class.getResource("Unit.png").toString(), 140, 200, false, false);
+        System.out.println(HelloApplication.class.getResource("Unit.png").toString());
+ ;
+        Text coordinations = new Text();
+        Pane pane = new Pane();
+        pane.getChildren().add(coordinations);
+        HelloApplication.group.getChildren().add(pane);
+        Scene scene = new Scene(group, 1000, 800);
+//------------------------------------------------------------------------------------------------------
+        //Створення нового об'єкту/зміна параметрів об'єкту
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (mouseEvent.getButton().equals(MouseButton.SECONDARY)){
+
+                    ChangeUnitParametrs.display(mouseEvent.getX(), mouseEvent.getY());
+
+                }
+                if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+                    CreateUnit.display(mouseEvent.getX(), mouseEvent.getY());
+                }
+            }
+        });
+        //Передача координат на поле
+        scene.setOnMouseMoved(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                coordinations.setText("x: " + mouseEvent.getX() + " y: " + mouseEvent.getY());
+                coordinations.setLayoutX(5);
+                coordinations.setLayoutY(10);
+
+            }
+        });
+//------------------------------------------------------------------------------------------------------
+
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
